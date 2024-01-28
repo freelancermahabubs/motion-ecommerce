238d13388e4b4a0a50a52370c5228ff72c6f85e1
smshaown <smshaown65@gmail.com>
2024-01-28 06:04:12 +0600

    profile done

--- js/main.js
+++ js/main.js
@@ -83,6 +83,7 @@
   initializeTabs("networkComponentsTabs");
   initializeTabs("healthOutdoorsTabs");
   initializeTabs("smartPhonesTabs");
+  initializeTabs("reviewsTabs");
 });
 
 
@@ -114,4 +115,97 @@ function togglePasswordVisibility(inputId, toggleIconId) {
       passwordInput.type = "password";
       togglePasswordIcon.src = "/image/signUp/eye.svg";
   }
+}
+
+
+// ======retailer-request start =====
+
+document.addEventListener('DOMContentLoaded', function () {
+  var checkbox = document.getElementById('sameAddressCheckbox');
+  var deliveryDetailsDiv = document.getElementById('deliveryDetails');
+
+  checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+      deliveryDetailsDiv.classList.add('hidden');
+    } else {
+      deliveryDetailsDiv.classList.remove('hidden');
+    }
+  });
+});
+
+
+
+
+// ======== Personal Info and update Personal Info ======= 
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the sections
+  const personalInfoSection = document.getElementById("personalInfoSection");
+  const updatePersonalInfoSection = document.getElementById("updatePersonalInfoSection");
+
+  // Get reference to the image icon
+  const profileEditIcon = document.getElementById("profileEditIcon");
+
+  // Add click event listener to the image icon
+  profileEditIcon.addEventListener("click", function (event) {
+    // Toggle visibility of the sections
+    personalInfoSection.classList.toggle("hidden");
+    updatePersonalInfoSection.classList.toggle("hidden");
+  });
+
+  // Add click event listener to the "Cancel" button in the update section
+  const cancelButton = document.getElementById("cancelButton");
+  cancelButton.addEventListener("click", function (event) {
+    // Prevent the default button behavior
+    event.preventDefault();
+
+    // Toggle visibility of the sections
+    personalInfoSection.classList.toggle("hidden");
+    updatePersonalInfoSection.classList.toggle("hidden");
+  });
+});
+
+
+
+
+// profile toggle left and right Side Bar Create the page start
+
+function changeContent(content, clickedLink) {
+  var rightSidebarContent = document.getElementById("right-sidebar-content");
+  var contentDiv = document.getElementById(content);
+
+  if (rightSidebarContent && contentDiv) {
+    var allContentDivs =
+      rightSidebarContent.getElementsByClassName("content-div");
+    for (var i = 0; i < allContentDivs.length; i++) {
+      allContentDivs[i].style.display = "none";
+    }
+
+    contentDiv.style.display = "block";
+
+    // Remove the 'active-link' class from all links
+    var allLinks = document.querySelectorAll(".submenu-link");
+    allLinks.forEach(function (link) {
+      link.classList.remove("active-link");
+    });
+
+    // Add the 'active-link' class to the clicked link
+    if (clickedLink) {
+      clickedLink.classList.add("active-link");
+    }
+  }
+}
+
+function changeTab(tabId) {
+  const tabButtons = document.querySelectorAll('.reviews-tab-button');
+  const tabContents = document.querySelectorAll('.reviews-tab-content');
+
+  tabButtons.forEach(button => button.classList.remove('active'));
+  tabContents.forEach(content => content.classList.remove('active'));
+
+  const clickedButton = document.getElementById(tabId.replace('content', 'tab'));
+  const clickedContent = document.getElementById(tabId);
+
+  clickedButton.classList.add('active');
+  clickedContent.classList.add('active');
 }
